8a4d882fa9ed0b2b01a6edb130abc6f07bdd6083
Mark Edward Ratio <markedwardratio@gmail.com>
2021-01-14 11:09:14 +0800

    Add user controller to return dummy content

---
+++ controllers/userController.js
@@ -0,0 +1,7 @@
+exports.createGet = (req, res, next) => {
+	res.send('NOT IMPLEMENTED: User create GET');
+};
+
+exports.createPost = (req, res, next) => {
+	res.send('NOT IMPLEMENTED: User create POST');
+};
