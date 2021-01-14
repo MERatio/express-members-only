6a6ea8424561c17e8c25edc2686eaac4c8d079d4
Mark Edward Ratio <markedwardratio@gmail.com>
2021-01-14 11:18:16 +0800

    Add users routes

--- routes/users.js
+++ routes/users.js
@@ -1,4 +1,11 @@
 const express = require('express');
 const router = express.Router();
 
+// Controller
+const userController = require('../controllers/userController');
+
+router.get('/create', userController.createGet);
+
+router.post('/create', userController.createPost);
+
 module.exports = router;
