c65f63659548fa486cea8f159f811d737652ff56
Mark Edward Ratio <markedwardratio@gmail.com>
2021-01-15 11:18:39 +0800

    Add index: true option to username of user schema

--- models/user.js
+++ models/user.js
@@ -5,7 +5,13 @@ const Schema = mongoose.Schema;
 const UserSchema = new Schema({
 	firstName: { type: String, required: true, maxlength: 255 },
 	lastName: { type: String, required: true, maxlength: 255 },
-	username: { type: String, required: true, maxlength: 20, unique: true },
+	username: {
+		type: String,
+		required: true,
+		maxlength: 20,
+		index: true,
+		unique: true,
+	},
 	password: { type: String, required: true },
 	member: { type: Boolean, default: false },
 });
