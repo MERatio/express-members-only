62981acd83b04b41e6239527b947b18eeea190e1
Mark Edward Ratio <markedwardratio@gmail.com>
2021-01-18 13:00:20 +0800

    Add post controller that returns dummy content

---
+++ controllers/postController.js
@@ -0,0 +1,7 @@
+exports.createGet = (req, res, next) => {
+	res.send('NOT IMPLEMENTED: Post create GET');
+};
+
+exports.createPost = (req, res, next) => {
+	res.send('NOT IMPLEMENTED: Post create POST');
+};
