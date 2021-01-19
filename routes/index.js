b77d7ffbe7241081c005ba619b1a84e38609b758
Mark Edward Ratio <markedwardratio@gmail.com>
2021-01-19 15:42:04 +0800

    Redirect '/' to '/posts'

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,8 @@
 const express = require('express');
 const router = express.Router();
 
-/* GET home page. */
 router.get('/', (req, res, next) => {
-	res.render('index', {
-		title: 'Express',
-		flashes: req.flash(),
-	});
+	res.redirect('/posts');
 });
 
 module.exports = router;
