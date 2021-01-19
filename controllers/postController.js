2764318d6449b48a373d69514afcbe878e4b0d47
Mark Edward Ratio <markedwardratio@gmail.com>
2021-01-19 12:48:25 +0800

    Add code for post controller createGet

--- controllers/postController.js
+++ controllers/postController.js
@@ -1,6 +1,11 @@
-exports.createGet = (req, res, next) => {
-	res.send('NOT IMPLEMENTED: Post create GET');
-};
+const beforeMiddleware = require('../lib/beforeMiddleware');
+
+exports.createGet = [
+	beforeMiddleware.authenticatedUser,
+	(req, res) => {
+		res.render('posts/form', { title: 'Create Post' });
+	},
+];
 
 exports.createPost = (req, res, next) => {
 	res.send('NOT IMPLEMENTED: Post create POST');
