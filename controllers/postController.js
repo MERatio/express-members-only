a13f1707bf91cac8c2f5c8b86f0ef313c6bf5c5e
Mark Edward Ratio <markedwardratio@gmail.com>
2021-01-19 15:22:29 +0800

    Add code for post controller createPost

--- controllers/postController.js
+++ controllers/postController.js
@@ -1,3 +1,9 @@
+const { body, validationResult } = require('express-validator');
+
+// Model
+const Post = require('../models/post');
+
+// Lib
 const beforeMiddleware = require('../lib/beforeMiddleware');
 
 exports.createGet = [
@@ -7,6 +13,43 @@ exports.createGet = [
 	},
 ];
 
-exports.createPost = (req, res, next) => {
-	res.send('NOT IMPLEMENTED: Post create POST');
-};
+exports.createPost = [
+	beforeMiddleware.authenticatedUser,
+	// Validate and sanitise fields.
+	body('title')
+		.trim()
+		.isLength({ max: 60 })
+		.withMessage('Title is too long (maximum is 60 characters)')
+		.escape(),
+	body('body')
+		.trim()
+		.isLength({ max: 1000 })
+		.withMessage('Body is too long (maximum is 1000 characters)')
+		.escape(),
+	// Process request after validation and sanitization.
+	(req, res, next) => {
+		// Extract the validation errors from a request.
+		const errors = validationResult(req);
+		if (!errors.isEmpty()) {
+			// There are errors. Render form again with sanitized values and error messages.
+			res.render('posts/form', {
+				title: 'Create Post',
+				user: req.body,
+				errors: errors.array(),
+			});
+		} else {
+			// Data form is valid.
+			// Create an Post object with escaped and trimmed data.
+			const post = new Post({
+				user: req.user,
+				title: req.title,
+				body: req.body,
+			});
+			post.save((err) => {
+				// Successful - redirect to root path.
+				req.flash('success', 'You have successfully create a new message.');
+				res.redirect('/');
+			});
+		}
+	},
+];
